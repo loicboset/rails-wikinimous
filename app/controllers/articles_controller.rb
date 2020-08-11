2dbc6a850241975a579828405618643081d8df10
Loic Boset <loic.boset@gmail.com>
2020-08-11 15:59:34 +0200

    Initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,37 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(article_params)
+    redirect_to articles_path
+  end
+
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+end
